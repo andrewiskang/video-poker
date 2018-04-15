5dcb32a4e5dd080955af310480674a1d804bae65
Andrew Kang <andrewiskang@gmail.com>
2018-04-14 20:12:23 -0700

    added Hand classes, fixed up organization and added more tests

--- tests.py
+++ tests.py
@@ -1,68 +1,150 @@
-import video_poker
+from video_poker import Card, Deck, Hand
+import random
 
 
-
-def success(total_tests, passed_tests):
+def success():
     # prints success
-    print("Test passed!")
-def failure(total_tests):
+    print("  Test passed!")
+def failure():
     # prints failure
-    print("Test failed!")
+    print("  Test failed!")
 
 
 
 
-def tests():
-    # function to hold all tests
+# keep track of all tests
+total = 0
+passed = 0
 
-    # keep track of all tests
-    total = 0
-    passed = 0
+# sample Cards
+sample_card_1 = Card("A", "Spades")
+sample_card_2 = Card("A", "Spades")
+sample_card_3 = Card("A", "Hearts")
+sample_card_4 = Card("9", "Hearts")
 
-    # sample cards
-    sample_card_1 = Card("A", "Spades")
-    sample_card_2 = Card("A", "Spades")
-    sample_card_3 = Card("A", "Hearts")
-    sample_card_4 = Card("9", "Hearts")
+# Card tests
+print("CARD TESTS")
 
-    # card tests
-    print("Card Tests")
+print (sample_card_1.info() + " == " + sample_card_2.info() + ":")
+if sample_card_1 == sample_card_2:
+    success()
+    total += 1
+    passed += 1
+else:
+    failure()
+    total += 1
 
-    print("card 1 == card 2")
-    if sample_card_1 == sample_card_2:
-        success(total, passed)
-    else:
-        failure(total)
+print (sample_card_1.info() + " != " + sample_card_3.info() + ":")
+if sample_card_1 != sample_card_3:
+    success()
+    total += 1
+    passed += 1
+else:
+    failure()
+    total += 1
 
-    print("card 1 != card 3")
-    if sample_card_1 != sample_card_3:
-        success(total, passed)
-    else:
-        failure(total)
+print (sample_card_1.info() + " != " + sample_card_4.info() + ":")
+if sample_card_1 != sample_card_4:
+    success()
+    total += 1
+    passed += 1
+else:
+    failure()
+    total += 1
 
-    print("card 1 != card 4")
-    if sample_card_1 != sample_card_4:
-        success(total, passed)
-    else:
-        failure(total)
+print (sample_card_3.info() + " != " + sample_card_4.info() + ":")
+if sample_card_3 != sample_card_4:
+    success()
+    total += 1
+    passed += 1
+else:
+    failure()
+    total += 1
 
-    print("card 3 != card 4")
-    if sample_card_3 != sample_card_4:
-        success(total, passed)
-    else:
-        failure(total)
+# Deck tests
+print("\nDECK TESTS")
 
-    # deck tests
-    print("\nDeck Tests")
+print("Full deck of cards contains the following:")
+new_deck = Deck.full_deck()
+for card in new_deck:
+    print("  "),
+    card.print_card()
 
-    deck = full_deck()
-    for card in deck:
-        card.print_card()
-    if len(deck) = 52:
-        success(total, passed)
-    else:
-        failure(total)
+print("Full deck of cards contains 52 cards:")
+if len(new_deck) == 52:
+    success()
+    total += 1
+    passed += 1
+else:
+    failure()
+    total += 1
 
-    print("\nTotal Tests:", str(total))
-    print("Passed Tests:", str(passed))
-    print("Failed Tests:", str(total - passed))
+print("Empty deck of cards contains 0 cards:")
+empty_deck = Deck()
+if len(empty_deck) == 0:
+    success()
+    total += 1
+    passed += 1
+else:
+    failure()
+    total += 1
+
+# Hand tests
+print("\nHAND TESTS")
+
+sample_deck = Deck.full_deck()
+sample_hand = sample_deck.draw_hand()
+print("Sample Hand:")
+print("  "),
+sample_hand.print_hand()
+print("After drawing, deck contains 47 cards:")
+if len(sample_deck) == 47:
+    success()
+    total += 1
+    passed += 1
+else:
+    failure()
+    total += 1
+
+print("\nWe are going to redraw all cards from an empty deck:")
+sample_hand = empty_deck.redraw(sample_hand, [0, 1, 2, 3, 4])
+print("  "),
+sample_hand.print_hand()
+print("After redrawing, empty deck continues to contain 0 cards:")
+if len(empty_deck) == 0:
+    success()
+    total += 1
+    passed += 1
+else:
+    failure()
+    total += 1
+
+print("\nWe are going to redraw the middle card from an empty deck:")
+sample_hand = empty_deck.redraw(sample_hand, [2])
+print("  "),
+sample_hand.print_hand()
+print("After redrawing, empty deck continues to contain 0 cards:")
+if len(empty_deck) == 0:
+    success()
+    total += 1
+    passed += 1
+else:
+    failure()
+    total += 1
+
+print("\nWe are going to redraw the middle card from the correct deck:")
+sample_hand = sample_deck.redraw(sample_hand, [2])
+print("  "),
+sample_hand.print_hand()
+print("After redrawing, deck continues to contains 47 cards:")
+if len(sample_deck) == 47:
+    success()
+    total += 1
+    passed += 1
+else:
+    failure()
+    total += 1
+
+print("\nTotal Tests: " + str(total))
+print("Passed Tests: " + str(passed))
+print("Failed Tests: " + str(total - passed))
--- video_poker.py
+++ video_poker.py
@@ -1,10 +1,10 @@
-
+import random
 
 
 class Card(object):
-    # a card has two characteristics: a rank (number) and a suit
+    # a Card has two characteristics: a rank (number) and a suit
     def __init__(self, rank, suit):
-        # return a Card with specified rank and suit
+        # initializes a Card with specified rank and suit
         self.rank = rank
         self.suit = suit
 
@@ -18,16 +18,66 @@ class Card(object):
         # overrides != function for comparison purposes
         return not self.__eq__(other)
 
-    def print_card(self):
-        # prints out Card rank and suit
-        print(self.rank + " of " + self.suit)
+    def info(self):
+        # returns card info as a string in the form "<rank> of <suit>"
+        return self.rank + " of " + self.suit
 
-def full_deck():
-    # returns a deck of 52 cards: 13 ranks of 4 suits each
-    deck = set()
-    ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
-    suits = {"Spades", "Hearts", "Clubs", "Diamonds"}
-    for rank in ranks:
-        for suit in suits:
-            deck.add(Card(rank, suit))
-    return deck
+    def print_card(self):
+        # prints out card info
+        print(self.info())
+
+class Deck(set):
+    # a Deck contains cards that can be drawn into or pulled out of a Hand
+    def random_draw(self):
+        # draw one Card from Deck at random
+        return random.sample(self, 1)[0]
+
+    def draw_hand(self):
+        # given a deck, draws a Hand of 5 Cards
+        hand = Hand()
+        new_cards = random.sample(self, 5)
+        for i in range(5):
+            hand.append(new_cards[i])
+            self.remove(new_cards[i])
+        return hand
+
+    def redraw(self, hand, indices):
+        # redraws Cards based on selected indices within the given Hand
+        # return all selected Cards back to the Deck before Cards are redrawn
+        for i in indices:
+            self.add(hand[i])
+
+        # if Card index was selected, redraw Card and remove from the Deck
+        # if Card index was not selected, keep same Card in Hand
+        new_hand = Hand()
+        for i in range(5):
+            if i in indices:
+                new_card = random.sample(self, 1)[0]
+                new_hand.append(new_card)
+                self.remove(new_card)
+            else:
+                new_hand.append(hand[i])
+
+        return new_hand
+
+    @staticmethod
+    def full_deck():
+    # returns a Deck of 52 cards: 13 ranks of 4 suits each
+        deck = Deck()
+        ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
+        suits = {"Spades", "Hearts", "Clubs", "Diamonds"}
+        for rank in ranks:
+            for suit in suits:
+                deck.add(Card(rank, suit))
+        return deck
+
+class Hand(list):
+    # a Hand contains 5 Cards from a Deck of 52 Cards, with no duplicates
+
+
+    def print_hand(self):
+        # prints out a visual representation of the Cards in the given Hand
+        card_info = []
+        for card in self:
+            card_info.append(card.info())
+        print("    ".join(card_info))
