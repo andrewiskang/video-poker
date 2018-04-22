5b7a35353ca2d33125457707a2b4813019f1149e
Andrew Kang <andrewiskang@gmail.com>
2018-04-22 16:52:18 -0700

    API requests now working properly, had to fix some bugs with deck and bankroll as well as routing

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, make_response, abort, jsonify
+from flask import Flask, make_response, abort, jsonify, request
 from video_poker2 import Card, Deck, Hand, Payout
 
 app = Flask(__name__)
@@ -36,21 +36,17 @@ def get_game(game_id):
         abort(404)
     return jsonify({'game' : game[0]})
 
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({"error" : "Not found"})), 404
-
 @app.route("/api/v1.0/games", methods=["POST"])
 def create_game():
-    if not request.json or not "payout" in request.json:
+    if not request.json or not "denomination" in request.json:
         abort(400)
     game = {
         "id": games[-1]["id"] + 1,
-        "deck": request.json.get("deck", []),
+        "deck": request.json.get("deck", Deck.new_deck()),
         "hand": [],
-        "payout": request.json["payout"],
+        "payout": request.json.get("payout", Payout.default()),
         "bankroll": request.json.get("bankroll", 1000),
-        "num_credits": request.json["num_credits"],
+        "num_credits": request.json.get("num_credits", 5),
         "denomination": request.json["denomination"],
         "has_redrawn": False
     }
@@ -65,10 +61,22 @@ def delete_game(game_id):
     games.remove(game[0])
     return jsonify({"success": True}), 204
 
-@app.route("/api/v1.0/games/<int:game_id>/play)", methods=["PUT"])
-def play(game_id):
+@app.route("/api/v1.0/games/<int:game_id>/play", methods=["GET"])
+def show_play(game_id):
+    game = [game for game in games if game["id"] == game_id]
+    if len(game) == 0:
+        abort(404)
+    hand = Hand(game[0]["hand"])
+    outcome = hand.outcome()
+    return jsonify({"hand": hand,
+                    "outcome": outcome})
+
+@app.route("/api/v1.0/games/<int:game_id>/play", methods=["PUT"])
+def draw_or_redraw(game_id):
     # deals a new hand or redraws certain cards based on has_redrawn
     game = [game for game in games if game["id"] == game_id]
+
+    # error checking
     if len(game) == 0:
         abort(404)
     if not request.json:
@@ -78,24 +86,45 @@ def play(game_id):
         if i not in range(5):
             abort(400)
 
+    # determine current stage of the game. if already redrawn, start a new game
+    # otherwise redraw based on the hold_indices
     num_credits = game[0]["num_credits"]
     denomination = game[0]["denomination"]
-    payout = game[0]["payout"]
-    if game[0]["has_redrawn"]:
+    payout = Payout(game[0]["payout"])
+    outcome = ""
+    if game[0]["has_redrawn"] or game[0]["hand"] == []:
+        # check if bankroll is large enough to place bet
+        if game[0]["bankroll"] < num_credits * denomination:
+            return jsonify({"success": False,
+                            "reason": "bankroll less than selected bet"}), 403
+        # otherwise place bet, shuffle deck, and draw new hand
+        # to ensure deck contains all cards, we re-initialize a new deck
         game[0]["bankroll"] -= num_credits * denomination
         game[0]["deck"] = Deck.new_deck()
         game[0]["hand"] = game[0]["deck"].draw_cards()
+        outcome = Hand(game[0]["hand"]).outcome()
         game[0]["has_redrawn"] = False
     else:
-        game[0]["hand"] = game[0]["deck"].draw_cards(game[0]["hand"], hold_indices)
+        # continue with the game, redrawing select cards based off hold_indices
+        # once cards are redrawn, determine outcome and pay user accordingly
+        deck = Deck(game[0]["deck"])
+        game[0]["hand"] = deck.draw_cards(game[0]["hand"], hold_indices)
+        game[0]["deck"] = deck
         outcome = Hand(game[0]["hand"]).outcome()
-        credits_won = payout.table[outcome] * num_credits
+        credits_won = payout[outcome] * num_credits
         game[0]["bankroll"] += credits_won * denomination
         game[0]["has_redrawn"] = True
 
+    # return current stage of the game (has_redrawn),
     return jsonify({"success": True,
-                    "game": game[0]}), 200
+                    "hand": game[0]["hand"],
+                    "outcome": outcome,
+                    "has_redrawn": game[0]["has_redrawn"],
+                    "bankroll": game[0]["bankroll"]}), 200
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({"error" : "Not found"})), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
