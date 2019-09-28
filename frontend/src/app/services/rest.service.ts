4797be0ba7ba9733cc4ec9920c5c8f670a680253
andrewiskang <andrewiskang@gmail.com>
2019-09-27 22:56:09 -0400

    adding game service

--- frontend/src/app/services/rest.service.ts
+++ frontend/src/app/services/rest.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestService {
 
-  constructor() { }
+  protected baseUrl: string = 'http://localhost:5000/api'
+
+  constructor(private http: HttpClient) { }
+
+  protected get(relativeUrl: string, responseType: any = 'json'): Observable<any> {
+    return this.http.get(this.baseUrl + relativeUrl, { responseType: responseType })
+  }
+
+  protected post(relativeUrl: string, data: any, responseType: any = 'json'): Observable<any> {
+    return this.http.post(this.baseUrl + relativeUrl, data, { responseType: responseType })
+  }
+
+  protected put(relativeUrl: string, data: any, responseType: any = 'json'): Observable<any> {
+    return this.http.put(this.baseUrl + relativeUrl, data, { responseType: responseType })
+  }
 }
