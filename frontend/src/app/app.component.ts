8cba253acf37a23cb19a3857da233197455191de
andrewiskang <andrewiskang@gmail.com>
2019-09-27 22:31:57 -0400

    cards are now showing up, although faces are still oversized and symbol position needs work

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,5 +6,86 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  hand: any[] = [
+      {
+          rank: {
+              name: 'seven',
+              symbol: '7',
+              value: 7
+          },
+          suit: {
+              name: 'heart',
+              symbol: '♥',
+              color: 'red'
+          }
+      },
+      {
+          rank: {
+              name: 'three',
+              symbol: '3',
+              value: 3
+          },
+          suit: {
+              name: 'heart',
+              symbol: '♥',
+              color: 'red'
+          }
+      },
+      {
+          rank: {
+              name: 'ace',
+              symbol: 'A',
+              value: 1
+          },
+          suit: {
+              name: 'heart',
+              symbol: '♥',
+              color: 'red'
+          }
+      },
+      {
+          rank: {
+              name: 'king',
+              symbol: 'K',
+              value: 13
+          },
+          suit: {
+              name: 'heart',
+              symbol: '♥',
+              color: 'red'
+          }
+      },
+      {
+          rank: {
+              name: 'ten',
+              symbol: '10',
+              value: 10
+          },
+          suit: {
+              name: 'heart',
+              symbol: '♥',
+              color: 'red'
+          }
+      }
+  ]
 }
+
+      //         symbol: '♣',
+      //     color: 'black'
+      //   },
+      //   diamond: {
+      //     name: 'diamond',
+      //     symbol: '♦',
+      //     color: 'red'
+      //   },
+      //   spade: {
+      //     name: 'spade',
+      //     symbol: '♠',
+      //     color: 'black'
+      //   },
+      //   heart: {
+      //     name: 'heart',
+      //     symbol: '♥',
+      //     color: 'red'
+      //   }
+      // };
