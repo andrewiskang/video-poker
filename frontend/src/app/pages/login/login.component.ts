20a6d3ede2eb969c0d42bd139ce9f8d6cc9d61f2
andrewiskang <andrewiskang@gmail.com>
2019-10-04 02:41:43 -0500

    added login frontend and functionality (only google signin works for now)

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service'
+import { Router } from '@angular/router'
+
+import { faGoogle, faFacebookSquare } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faPhoneAlt  } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +13,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  faGoogle = faGoogle
+  faFacebookSquare = faFacebookSquare
+  faEnvelope = faEnvelope
+  faPhoneAlt = faPhoneAlt
+
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+    this.authService.user.subscribe(user => {
+      if (user) {
+        console.log('already logged in')
+        this.router.navigateByUrl('/')
+      }
+    })
   }
 
+  googleLogin() {
+    this.authService.googleLogin()
+  }
 }
