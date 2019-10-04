50cacc3a0b2d276c7923be3fcf14b6256e9ba57a
andrewiskang <andrewiskang@gmail.com>
2019-10-03 23:33:42 -0500

    added firebase env

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCiZCbJyZm8VO4NKmiqHPA1EBntCLgktiQ",
+    authDomain: "video-poker-202000.firebaseapp.com",
+    databaseURL: "https://video-poker-202000.firebaseio.com",
+    projectId: "video-poker-202000",
+    storageBucket: "video-poker-202000.appspot.com",
+    messagingSenderId: "193706581223",
+    appId: "1:193706581223:web:789112463eb8ed5e2e5642",
+    measurementId: "G-MKBPLEG3JB"
+  }
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCiZCbJyZm8VO4NKmiqHPA1EBntCLgktiQ",
+    authDomain: "video-poker-202000.firebaseapp.com",
+    databaseURL: "https://video-poker-202000.firebaseio.com",
+    projectId: "video-poker-202000",
+    storageBucket: "video-poker-202000.appspot.com",
+    messagingSenderId: "193706581223",
+    appId: "1:193706581223:web:789112463eb8ed5e2e5642",
+    measurementId: "G-MKBPLEG3JB"
+  }
 };
 
 /*
