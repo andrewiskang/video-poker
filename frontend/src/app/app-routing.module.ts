e9faebd1c5a0356623462d4e5550941cceb113d6
andrewiskang <andrewiskang@gmail.com>
2019-10-03 23:33:25 -0500

    added login and (default) game routes, with authguard

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './services/auth.guard';
+import { LoginComponent } from './pages/login/login.component';
+import { GameComponent } from './pages/game/game.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '**',
+    component: GameComponent,
+    canActivate: [AuthGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
